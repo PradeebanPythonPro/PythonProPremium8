aeb2fa4f9b6dc80d1ea1e23526dbc769647bed7f
PradeebanPythonPro <166913200+PradeebanPythonPro@users.noreply.github.com>
2024-09-21 11:00:44 +0200

    Create meme_dict.py

---
+++ M1L1/meme_dict.py
@@ -0,0 +1,15 @@
+meme_dict = {
+            "CRINGE": "Qualcosa di eccezionalmente strano o imbarazzante",
+            "LOL": "Una risposta comune a qualcosa di divertente",
+            "SHEESH": "leggera disapprovazione",
+            "CREEPY" : "spaventoso, inquietante",
+            "PARA" : "preoccuparsi per qualcosa, paranoiarsi"
+            }
+
+parola = (input("Scrivi una parola che non capisci:")).upper()
+if parola in meme_dict.keys():
+    # Cosa fare se la parola è stata trovata?
+    print(meme_dict[parola])
+else:
+    # Cosa fare se la parola non è stata trovata?
+    print("La parola cercata non è presente nel dizionario")
